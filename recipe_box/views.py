088988bd1d289d56ac1a6761af4150fd92f776da
DavidRinSE <davidrichardsonin@gmail.com>
2020-05-08 16:44:00 -0400

    Completed basic assignment

--- recipe_box/views.py
+++ recipe_box/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
-
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from recipe_box.models import Recipe, Author
-from recipe_box.forms import AuthorAddForm, RecipeAddForm
+from recipe_box.forms import AuthorAddForm, RecipeAddForm, LoginForm
 # Create your views here.
+
+
+def loginview(request):
+    html = "loginpage.html"
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request,
+                username=data['username'],
+                password=data['password']
+            )
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(
+                    request.GET.get('next', reverse('homepage'))
+                )
+    form = LoginForm()
+    return render(request, html, {"form": form})
+
+
+def logoutview(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('homepage'))
 
 
 def index(request):
@@ -30,23 +58,37 @@ def recipe(request, id=0):
         })
 
 
+@login_required
 def add_author(request):
-    html = "add_author.html"
+    if request.user.is_staff:
+        html = "add_author.html"
 
-    if request.method == "POST":
-        form = AuthorAddForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            Author.objects.create(
-                name=data["name"],
-                bio=data["bio"]
-            )
-            return HttpResponseRedirect(reverse("homepage"))
-    else:
-        form = AuthorAddForm()
-        return render(request, html, {"form": form})
+        if request.method == "POST":
+            form = AuthorAddForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user = User.objects.create_user(
+                    username=data["username"],
+                    password=data["password"]
+                )
+                Author.objects.create(
+                    name=data["name"],
+                    bio=data["bio"],
+                    user=user
+                )
+                return HttpResponseRedirect(reverse("homepage"))
+        else:
+            form = AuthorAddForm()
+            return render(request, html, {"form": form})
+    html = "error.html"
+    return render(
+        request,
+        html,
+        {"error": "You cannot create an author without a staff account."}
+    )
 
 
+@login_required
 def add_recipe(request):
     html = "add_recipe.html"
 
