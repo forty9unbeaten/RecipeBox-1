a2279900d9f79ee5b7c682ff07693e210e36f478
DavidRinSE <davidrichardsonin@gmail.com>
2020-05-08 15:54:36 -0400

    fixed urls

--- recipe_box/urls.py
+++ recipe_box/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name="homepage"),
     path('author/<int:id>', views.author),
     path('recipe/<int:id>', views.recipe),
-    path('addauthor', views.add_author),
-    path('addrecipe', views.add_recipe)
+    path('addauthor/', views.add_author),
+    path('addrecipe/', views.add_recipe)
 ]
