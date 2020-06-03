0071c8d8d27abe47f67c09ac94102b026380daca
Rob Spears <rob.sp3ars@gmail.com>
2020-06-02 21:27:45 -0400

    Add url path to add and remove recipe as favorite

--- recipe_box/urls.py
+++ recipe_box/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('author/<int:id>', views.author, name='author_detail'),
     path('recipe/<int:id>', views.recipe, name='recipe_detail'),
     path('recipe/<int:id>/edit', views.edit_recipe_view, name='edit_recipe'),
+    path('recipe/<int:id>/save', views.favorite_view, name='save_recipe'),
+    path('recipe/<int:id>/remove', views.remove_favorite_view, name='remove_recipe'),
     path('addauthor/', views.add_author),
     path('addrecipe/', views.add_recipe),
     path('login/', views.loginview),
