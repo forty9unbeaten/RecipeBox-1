0254d28ab5148ec0582cfa33614b3d910d9001c9
Rob Spears <rob.sp3ars@gmail.com>
2020-06-02 20:24:50 -0400

    Add names to author detail and recipe detail url paths; add url path to edit recipes

--- recipe_box/urls.py
+++ recipe_box/urls.py
@@ -3,8 +3,9 @@ from recipe_box import views
 
 urlpatterns = [
     path('', views.index, name="homepage"),
-    path('author/<int:id>', views.author),
-    path('recipe/<int:id>', views.recipe),
+    path('author/<int:id>', views.author, name='author_detail'),
+    path('recipe/<int:id>', views.recipe, name='recipe_detail'),
+    path('recipe/<int:id>/edit', views.edit_recipe_view, name='edit_recipe'),
     path('addauthor/', views.add_author),
     path('addrecipe/', views.add_recipe),
     path('login/', views.loginview),
